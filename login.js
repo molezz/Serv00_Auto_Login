11f8385eb2e5820ad3bcedc5375f6b82f376e461
molezz <molezz@gmail.com>
2024-06-18 13:24:06 +0800

    Update login.js
    panel适配更多域名

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ async function delayTime(ms) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    let url = `https://panel${panelnum}.serv00.com/login/?next=/`;
+    let url = `https://${panelnum}/login/?next=/`;
 
     try {
       // 修改网址为新的登录页面
